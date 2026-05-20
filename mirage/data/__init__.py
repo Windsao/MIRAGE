5cd16212cc0b4fdf0982b63451daee937894398b
Shang Wu <shangwu2028@u.northwestern.edu>
2026-05-20 15:55:41 -0500

    fix: export libero_spatial_dataset factory

--- mirage/data/__init__.py
+++ mirage/data/__init__.py
@@ -1,5 +1,5 @@
 """Dataset loaders for embodied SFT/RL."""
 
-from .libero_dataset import LiberoActionDataset
+from .libero_dataset import LiberoActionDataset, libero_spatial_dataset
 
-__all__ = ["LiberoActionDataset"]
+__all__ = ["LiberoActionDataset", "libero_spatial_dataset"]
